b53fb983af96f296b913d03e11217c1738502afc
Vladimir <guiderwg@gmail.com>
2020-02-04 16:56:18 +0400

    add header

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+	var searchBtn = $('.search-btn'),
+			closeBtn = $('.search__close'),
+			search = $('.search');
+
+	searchBtn.on("click", function (e) {
+		e.preventDefault();
+		search.toggleClass('search_active');
+
+		closeBtn.click(function() {
+			search.removeClass('search_active');
+		});
+	});
+
 	$('[data-toggle="popover"]').popover();
 	$(".owl-carousel").owlCarousel();
 	$("a[rel^='fancyimg']").fancybox({});
